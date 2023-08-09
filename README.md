20b8422c46945126a7cfa98b2847c17512b83927
Przemek Sekula <psekula@umd.edu>
2023-08-09 18:19:51 -0400

    feat: first version

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# RitisChatBot
+# RITIS Chat Bot
+---
+
+This is the initial work on RITIS Chat Bot. This bot is suppose to be an assistant that guides people through their daily work.
+
+### Installation
+Create your own conda environment, for example:
+```bash
+conda create --name ritis python==3.11
+conda activate ritis
+```
+
+Install the requirements with
+```bash
+pip install -r requirements.txt
+```
+
+Create `config.py` in parent directory. If you want to use ChatGPT you should define proper `OPENAI_KEY` in this file. If you want to debug your pipelines with LangChain, you should define proper `LANGCHAIN_KEY` (normally not required)
+
+Example of `config.py`:
+```bash
+OPENAI_KEY = '<YOUR KEY HERE>'
+LANGCHAIN_KEY = '<YOUR KEY HERE>'
+```
+
+Run the `01_CreateVectorStoreOpenAI.ipynb` notebook to build a database.
+
+Run the chatbot with:
+```bash
+python ritis_chatbot.py
+```
+
+### Content
+- `Main folder`:
+    - Code for creating vector stores (for now: `01_CreateVectorStoreOpenAI.ipynb`)
+    - Code that runs a chatbot (for now: `ritis_chatbot.py`)
+    - `config.py` (not stored on GitHub)
+- `chroma/`: folder with a vector store. Not stored on GitHub. Generated automatically
+- `flagged/`: Folder with gradio flags. Not stored on GitHub. Generated automatically
+- `input_texts/`: Input data 
+- `tests`: Various notebooks for test purposes
+
+
+### TODO:
+- Add memory
+- Create ChatGPT-independent versions
+
+
+
---
+++ ritis_chatbot.py
@@ -0,0 +1,94 @@
+import os
+import sys
+
+import openai
+import config
+
+from langchain.vectorstores import Chroma
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+
+from transformers import pipeline
+import gradio as gr
+
+
+def transcribe_audio(mic):
+
+    transcription = audio_model(mic)["text"]
+    return transcription
+
+
+def ask_question(question = None, mic = None):
+    if mic is None:
+        input_text = question
+    else:
+        input_text = transcribe_audio(mic=mic)
+        
+    result = qa({"query": input_text})
+    
+    res = """
+    Question: {}\n
+    Answer: {}\n
+    """.format(input_text, result['result'])
+    
+    return res
+
+
+if __name__ == "__main__":
+    openai.api_key  = config.OPENAI_KEY
+    os.environ['OPENAI_API_KEY'] = config.OPENAI_KEY
+
+
+    audio_model = pipeline("automatic-speech-recognition")
+
+    LLM_NAME = "gpt-3.5-turbo"
+
+    llm = ChatOpenAI(model_name=LLM_NAME, temperature=0)
+    persist_directory = './chroma'
+    embedding = OpenAIEmbeddings()
+    vectordb = Chroma(persist_directory=persist_directory, embedding_function=embedding)
+
+    template = """
+    The following pieces of texts contain framgents from RITIS tutorials.
+    Use these texts to addres the question and give the correct answer. 
+    If you don't know the answer, just say that you don't know, don't try to make up an answer. 
+    Use ten sentences maximum. 
+    {context}
+    Question: {question}
+    Helpful Answer:"""
+    QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context", "question"],template=template,)
+
+    question = "What tool can I use to rank bottlenecks?"
+    qa= RetrievalQA.from_chain_type(
+        llm,
+        retriever=vectordb.as_retriever(),
+        return_source_documents=True,
+        chain_type_kwargs={"prompt": QA_CHAIN_PROMPT})
+
+    with open('app_description.html', 'r') as f:
+        app_description = f.read()
+
+    gr.Interface(
+        fn=ask_question,
+        inputs=[
+            gr.inputs.Textbox(label="Question", optional=True),
+            gr.Audio(source="microphone", type="filepath", optional=True),
+        ],
+        outputs="text",
+        
+        title="RITIS chatbot - v 0.01",  
+        description=app_description,
+        
+        
+        examples = [
+            ["What tool can I use to rank bottlenecks?", None],
+            ["how can I ask for CO2 emissions in College Park", None],
+            ["How to estimate the delay costs", None],
+            ["I need to analyze carbon dioxide emissions " \
+                "in 2020 at I-95 corridor and check if there " \
+                "were any anomalies. Can I do it with RITIS?", None],
+            ["Give me all the tools that can analyze carbon dioxide emissions.", None],
+        ]
+    ).launch(server_name="0.0.0.0", server_port=8891)
